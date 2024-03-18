d690530d1e9597c45dbfbbcf66c100c0671adfb0
Sileshi Abebe <abebesileshi828@gmail.com>
2024-03-18 23:48:35 +0300

    alternative way of serialization

--- home/serializers.py
+++ home/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
-from .models import Person
+from .models import Person,Color
+
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        fields = ['color_name']
 
 class PersonSerializer(serializers.ModelSerializer):
+    color = ColorSerializer()
+    color_inf = serializers.SerializerMethodField()
     class Meta:
         model = Person
         fields = '__all__'
+    
+
+    def get_color_inf(self,obj):
+        return {'color_name':Color.objects.get(id = obj.color.id).color_name,'hex':'#005'}
     def validate(self, data):
         special_characters = set(['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', '|', '\\', ';', ':', "'", '"', ',', '.', '<', '>', '/', '?'])
         for c in data['name']:
