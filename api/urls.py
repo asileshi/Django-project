05ac4c79cb6a1497f1da1e8dd30ed1cc553f805f
Sileshi Abebe <abebesileshi828@gmail.com>
2024-03-20 13:24:06 +0300

    Loging serializer completed

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from home import views
 from django.urls import path
 urlpatterns = [
     path('index/', views.index,name=''),
-    path('person/',views.person,name='person/')
+    path('person/',views.person,name='person/'),
+    path('login/',views.login,name='login')
 ]
